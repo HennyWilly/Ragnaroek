41f26168c10842d45743fc4ce971d1d548b32dd4
rn869565 <rn869565@zam1060>
2014-05-21 14:24:08 +0200

    Hallo Leute

--- src/ai/SimpleAI.java
+++ src/ai/SimpleAI.java
@@ -74,8 +74,8 @@ public class SimpleAI implements AI {
 				// Spieler steht bereits auf Schatz, z.B. wenn herausgeschoben
 				// TODO Random Shift
 				
-				int index = 2 * (rand.nextInt() % 3) + 1;
-				int side = rand.nextInt(2);
+				int index = 2 * (rand.nextInt() % 3) + 1; //->1,3,5
+				int side = rand.nextInt(4);  // ->0,1,2,3
 				
 				int col = -1;
 				int row = -1;
